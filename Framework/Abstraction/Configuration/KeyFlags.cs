8d6ec0366ddc92f3d6959fbc365b15a1540f9d6e
HolyshitOvO <39211450+HolyshitOvO@users.noreply.github.com>
2023-09-03 19:30:23 +0800

    2023-09-03

--- Framework/Abstraction/Configuration/KeyFlags.cs
+++ Framework/Abstraction/Configuration/KeyFlags.cs
@@ -2,14 +2,19 @@
 
 namespace HakeQuick.Abstraction.Base
 {
+    /// <summary>
+    /// https://learn.microsoft.com/en-us/windows/win32/inputdev/wm-hotkey
+    /// </summary>
     [Flags]
     public enum KeyFlags : uint
     {
         NONE = 0x0,
-        ALT = 0x1,
+        NOREPEAT = 0x4000,
+
+        CTRL = 0x2,
         CONTROL = 0x2,
         SHIFT = 0x4,
+        ALT = 0x1,
         WIN = 0x8,
-        NOREPEAT = 0x4000,
     }
 }
