7a6b4b2b128bca343b889c8985ae384617aa46e2
CandyTek <39211450+CandyTek@users.noreply.github.com>
2024-12-28 17:06:41 +0800

    更新程序集信息

--- CandyLauncher/Properties/AssemblyInfo.cs
+++ CandyLauncher/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CandyLauncher base HakeQuick")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("糖果启动器")]
+[assembly: AssemblyDescription("CandyLauncher base HakeQuick")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CandyTek")]
 [assembly: AssemblyProduct("CandyLauncher")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2025 CandyTek")]
+[assembly: AssemblyTrademark("CandyTek")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
